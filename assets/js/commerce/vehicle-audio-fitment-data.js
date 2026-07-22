85066ed98ccecfdd8376c411e84f2b2e15992dc4
Deans Car Audio <deans@local>
2026-07-21 20:22:56 -0700

    Add 24 more truck speaker box listings (30 total) with images and fitment data file

---
+++ assets/js/commerce/vehicle-audio-fitment-data.js
@@ -0,0 +1,30 @@
+(function () {
+  "use strict";
+
+  window.DCA_VEHICLE_AUDIO_FITMENT_DATA = Object.freeze([
+    Object.freeze({
+      id: "aston-martin-dbx-2020-2024",
+      make: "aston martin",
+      models: Object.freeze(["dbx", "dbx707", "dbx 707"]),
+      yearStart: 2020,
+      yearEnd: 2024,
+      sourceStatus: "Manufacturer documented",
+      verifiedAt: "2026-07-21",
+      speakers: Object.freeze({
+        dash: "100 mm (3.9 in) + 25 mm (1 in)",
+        front: "100 mm (3.9 in) + 160 mm (6.3 in)",
+        rear: "25 mm (1 in) + 160 mm (6.3 in)",
+      }),
+      sources: Object.freeze([
+        Object.freeze({
+          label: "Aston Martin DBX Owner's Guide - Media Systems 8.2-8.3",
+          url: "https://www.astonmartin.com/guides/MY83-19A321-HA.pdf",
+        }),
+        Object.freeze({
+          label: "Aston Martin DBX technology specification",
+          url: "https://media.astonmartin.com/aston-martin-unveils-dbx-an-suv-with-the-soul-of-a-sports-car-3/?lang=eng",
+        }),
+      ]),
+    }),
+  ]);
+})();
